1371a933fe7c4a4a241cf8f468549382296186b1
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-05-16 00:19:40 +0700

    [PLAY_SONG] feat(urls): add path for songs
    list of songs

--- play_song/urls.py
+++ play_song/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from play_song.views import play_song, add_song_playlist, download_song
+from play_song.views import play_song, add_song_playlist, download_song, songs
 
 app_name = 'play_song'
 
 urlpatterns = [
     path('', play_song, name='play_song'),
+    path('songs/', songs, name='songs'),
     path('add_song_playlist/', add_song_playlist, name='add_song_playlist'),
     path('download_song/', download_song, name='download_song'),
 ]
