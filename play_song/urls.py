c382e5424f10199c685c610f767dfcd46a8e277a
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-05-16 23:50:42 +0700

    [PLAY_SONG] feat(views. urls): delete add_song_playlist feature

--- play_song/urls.py
+++ play_song/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from play_song.views import play_song, add_song_playlist, songs
+from play_song.views import play_song, songs
 
 app_name = 'play_song'
 
 urlpatterns = [
     path('<str:song_id>/', play_song, name='play_song'),
     path('songs/', songs, name='songs'),
-    path('add-song-playlist/', add_song_playlist, name='add_song_playlist'),
 ]
